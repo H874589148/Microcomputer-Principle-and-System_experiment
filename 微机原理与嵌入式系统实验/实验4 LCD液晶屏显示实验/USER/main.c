a01b9842d738238c4c82d3845441eed36e49aa66
Hawkeye <40380233+H874589148@users.noreply.github.com>
2020-09-25 21:50:08 +0800

    实验4

---
+++ 微机原理与嵌入式系统实验/实验4 LCD液晶屏显示实验/USER/main.c
@@ -0,0 +1,41 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "12864.h"
+#include "stm32f4xx.h"
+#include "KEY.h"
+
+int main(void)
+{ 
+ 
+	delay_init(168);		  //初始化延时函数
+//	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+  pcb_Init();
+  delay_ms(20);
+  lcd_clear();
+  delay_ms(10);
+	KEY_Init();
+	
+	while(1)
+	{ if(DIP0==1){
+			 	lcd_wstr(1,1,"ST STM32F407");
+				lcd_wstr(2,1," Cortex-M4");
+				lcd_wstr(3,1," ARM实验箱");
+				lcd_wstr(4,0,"皮赛12电子有限公司");
+	}
+		else {
+			lcd_clear();
+			while(1){
+				delay_ms(20);
+					lcd_wstr(2,1," Cortex-M4");
+				if(DIP0==1)
+				{
+					lcd_clear();					
+				break; 
+				}
+		}
+
+	}
+
+}
+	}
